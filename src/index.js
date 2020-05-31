08eb7de2d133f48e4ab4eb1ad4ebb02481f366d6
ttak0422 <bgdaewalkman@gmail.com>
2020-05-31 13:48:59 +0900

    :sparkles: hit-testを実装

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import * as THREE from "three"
 import { ARButton } from "./lib/ar-button";
 
-
 // three
 let renderer, camera, light, reticle, object, scene;
+let objects = [];
+const MAX_OBJECT = 10;
 
 const startTHREE = () => {
     const w = window.innerWidth;
@@ -17,19 +18,25 @@ const startTHREE = () => {
     camera = new THREE.PerspectiveCamera(60, w / h, 0.1, 100);
     // light
     light = new THREE.DirectionalLight(0xffffff, 0.7);
-    light.position.set(1,1,1).normalize();
+    light.position.set(1, 1, 1).normalize();
     // reticle
     const innerRadius = 0.04;
     const outerRadius = 0.05;
     const thetaSegments = 30;
     const phiSegments = 1;
     const reticleGeometry = new THREE.RingGeometry(innerRadius, outerRadius, thetaSegments, phiSegments);
-    const reticleMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
+    const reticleMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
     reticle = new THREE.Mesh(reticleGeometry, reticleMaterial);
-    reticle.rotation.set(-Math.PI/2,0,0);
+    reticle.rotation.set(-Math.PI / 2, 0, 0);
     reticle.visible = false;
-    // object(uzimal)
-    // TODO:
+    // object
+    // TODO: uzimalにする
+    const objectRadius = 0.1;
+    const objectWidithRevisions = 32;
+    const objectHeightRevisions = 16;
+    const objectGeometry = new THREE.SphereBufferGeometry(objectRadius, objectWidithRevisions, objectHeightRevisions);
+    const objectMaterial = new THREE.MeshPhongMaterial();
+    object = new THREE.Mesh(objectGeometry, objectMaterial);
     // scene
     scene = new THREE.Scene();
     scene.add(light);
@@ -37,34 +44,78 @@ const startTHREE = () => {
 }
 
 const update = () => {
+    xrSession.requestAnimationFrame((time, frame) => {
+        const pose = frame.getViewerPose(xrRefSpace);
+        reticle.visible = false;
+        if (xrHitTestSource && pose) {
+            const hitTestResults = frame.getHitTestResults(xrHitTestSource);
+            if (hitTestResults.length > 0) {
+                reticle.visible = true;
+                const pose = hitTestResults[0].getPose(xrRefSpace);
+                const position = pose.transform.position;
+                reticle.position.set(position.x, position.y, position.z);
+            }
+        }
+    });
     renderer.render(scene, camera);
+}
+
+const onSelect = () => {
+    if (reticle.visible) {
+        generateObject();
+    }
+}
+
+const generateObject = () => {
+    console.log("generateObject");
+    let obj = object.clone();
+    obj.position.set(reticle.position.x, reticle.position.y + 0.05, reticle.position.z);
+    scene.add(obj);
+    if (objects.length > MAX_OBJECT) {
+        let o = objects.shift();
+        scene.remove(o);
+    }
 }
 
 // xr session
 let xrSession;
+let xrRefSpace;
+let xrViewerSpace;
+let xrHitTestSource;
 const onRequestSession = () =>
     navigator.xr.requestSession(
         "immersive-ar", {
-        requestFeatures: ["local"],
+        requiredFeatures: ["local", "hit-test"],
     }
     ).then(onStartSession); // TODO: onRequestSessionError...
 
 const onStartSession = (session) => {
+    session.addEventListener('select', onSelect);
     // three
-    renderer.xr.enable = true;
+    renderer.xr.enabled = true;
     renderer.xr.setReferenceSpaceType("local");
     renderer.xr.setSession(session);
-    
+    // session
     xrSession = session;
-    renderer.setAnimationLoop(update);
+    xrSession.requestReferenceSpace("viewer").then(refSpace => {
+        xrViewerSpace = refSpace;
+        xrSession.requestHitTestSource({ space: xrViewerSpace }).then(hitTestSource => {
+            xrHitTestSource = hitTestSource;
+        });
+    });
+    xrSession.requestReferenceSpace("local").then(refSpace => {
+        xrRefSpace = refSpace;
+        renderer.setAnimationLoop(update);
+    });
 }
 
 // TODO:
-const onEndSession = () => { }
+const onEndSession = () => {
+}
 
 // Entry point
 window.onload = () => {
     startTHREE();
-    const arButton = new ARButton({onRequestSession});
+    const arButton = new ARButton({ onRequestSession });
     document.body.appendChild(arButton);
 }
